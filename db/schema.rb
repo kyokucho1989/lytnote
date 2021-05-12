f1620835c23d54d5c7f2073b93f95b1920bb0a97
ko <kyokucho1989@gmail.com>
2021-05-13 05:26:22 +0900

    status追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_202333) do
+ActiveRecord::Schema.define(version: 2021_05_12_202521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2021_05_12_202333) do
   create_table "plan_review_lists", force: :cascade do |t|
     t.integer "plan_id"
     t.integer "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
