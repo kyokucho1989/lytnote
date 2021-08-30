71538b365b86408b0ab5ec9f196468f5338566d2
ko <kyokucho1989@gmail.com>
2021-08-31 05:36:19 +0900

    varidate変更

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,7 +2,7 @@ class Plan < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   has_many :plan_reviews
-  varidates :deadline_cannot_set_in_past
+  validate :deadline_cannot_set_in_past
   validates :name, length: { maximum: 20 }
   def deadline_cannot_set_in_past
     if deadline.present? $$ deadline < Date.today
