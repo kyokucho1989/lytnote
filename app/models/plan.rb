d2730fdbe7a298458a6cc7e204dfb79423a654b9
ko <kyokucho1989@gmail.com>
2021-09-01 05:48:36 +0900

    バリエーションエラー修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,8 +5,8 @@ class Plan < ApplicationRecord
   validate :deadline_cannot_set_in_past
   validates :name, length: { maximum: 20 }
   def deadline_cannot_set_in_past
-    if deadline.present? $$ deadline < Date.today
-      erros.add(:deadline, ": 過去の日付は使えません")
+    if deadline.present? && deadline < Date.today
+      errors.add(:deadline, ": 過去の日付は使えません")
     end
   end
 end
