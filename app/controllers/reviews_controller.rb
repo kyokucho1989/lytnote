41c113a0058f9bfce3e83a6b6ea8d36cb7b8066a
ko <kyokucho1989@gmail.com>
2021-09-08 05:39:00 +0900

    top修正

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
