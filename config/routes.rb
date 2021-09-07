4fbb4f7ba8f11b96488ba1f7be24e6a292d309fe
ko <kyokucho1989@gmail.com>
2021-09-08 05:28:15 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "plans#index"
+  # root "plans#index"
   resources :plans
+  resources :reports
+  resources :reviews
 end
