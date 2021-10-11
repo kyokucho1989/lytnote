9110a0194143400ac7a35f55137908d03c8672f7
ko <kyokucho1989@gmail.com>
2021-10-12 05:26:22 +0900

    report-rspec

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
+# t.references :user, null: false, foreign_key: true
+# t.datetime :reported_on
+# t.string :content
+
 RSpec.describe Report, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "reported_onを指定していない" do
+    it "エラーが出る" do
+      user = User.new(email: "satou@example.com", password: "password")
+      report = Report.new(reported_on:nil,content: "忙しかったけど学習できた。",user_id:user.id)
+      report.valid?
+      expect(report.errors.messages[:user]).to include "を入力してください"
+    end
+  end
+
+  context "contentがない" do
+    it "エラーが出る" do
+    end
+  end
+
+  context "reported_onが同じ日で存在する（1日1つ)" do
+    it "エラーが出る" do
+    end
+  end
 end
