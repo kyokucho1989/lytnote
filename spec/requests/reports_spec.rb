2082470c9140a9a2b6a50a3315637a9a475b1e99
ko <kyokucho1989@gmail.com>
2021-10-19 05:13:49 +0900

    report request spec

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Reports", type: :request do
+  before do
+    @user = create(:user)
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/reports/index"
+      sign_in @user
+      get reports_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
+      sign_in @user
       get "/reports/show"
       expect(response).to have_http_status(:success)
     end
@@ -17,6 +23,7 @@ RSpec.describe "Reports", type: :request do
 
   describe "GET /new" do
     it "returns http success" do
+      sign_in @user
       get "/reports/new"
       expect(response).to have_http_status(:success)
     end
@@ -24,6 +31,7 @@ RSpec.describe "Reports", type: :request do
 
   describe "GET /create" do
     it "returns http success" do
+      sign_in @user
       get "/reports/create"
       expect(response).to have_http_status(:success)
     end
@@ -31,6 +39,7 @@ RSpec.describe "Reports", type: :request do
 
   describe "GET /edit" do
     it "returns http success" do
+      sign_in @user
       get "/reports/edit"
       expect(response).to have_http_status(:success)
     end
@@ -38,6 +47,7 @@ RSpec.describe "Reports", type: :request do
 
   describe "GET /destroy" do
     it "returns http success" do
+      sign_in @user
       get "/reports/destroy"
       expect(response).to have_http_status(:success)
     end
@@ -45,6 +55,7 @@ RSpec.describe "Reports", type: :request do
 
   describe "GET /update" do
     it "returns http success" do
+      sign_in @user
       get "/reports/update"
       expect(response).to have_http_status(:success)
     end
