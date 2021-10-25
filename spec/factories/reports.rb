405bfb9af64aaa0adc5ce482683d1931234a9c62
ko <kyokucho1989@gmail.com>
2021-10-26 05:49:53 +0900

    factory bot データ修正

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :report do
     user { nil }
-    created_on { "2021-08-27 05:25:38" }
+    created_on {Faker::Date.between(from: 2.days.ago, to: Date.today)}
     content { "MyString" }
   end
 end
