16588e745aebff883270bdfdbef06ad223d9ff33
ko <kyokucho1989@gmail.com>
2021-10-27 05:40:37 +0900

    model spec追加

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :report do
-    user { nil }
-    created_on {Faker::Date.between(from: 2.days.ago, to: Date.today)}
+    reported_on {Faker::Date.between(from: 2.days.ago, to: Date.today)}
     content { "MyString" }
   end
 end
