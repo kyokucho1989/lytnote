a374cdeeec5bdf0988e81de720214c7cbeb693b4
ko <kyokucho1989@gmail.com>
2021-10-27 22:20:27 +0900

    共有文章作成

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,4 +4,15 @@ class Report < ApplicationRecord
   accepts_nested_attributes_for :report_items
   validates :reported_on, presence: true, uniqueness: { scope: :user }
   validates :content, length: { maximum: 200 }, presence: true
+  validates :content_for_share, length: { maximum: 200 }
+
+
+  def self.convert_content_shared(formatted_para)
+    date = formatted_para[:reported_on]
+    items = formatted_para[:report_items_attributes]
+    content = formatted_para[:content]
+    binding.pry
+    share_content = date + items + content
+    share_content
+  end
 end
