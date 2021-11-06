2f93380bddf2982560fd5c2d3401847469392567
ko <kyokucho1989@gmail.com>
2021-11-07 06:16:09 +0900

    model spec修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
   has_many :report_items, dependent: :restrict_with_exception
   has_many :plans, dependent: :restrict_with_exception
   belongs_to :user
-  validates :name, length: { maximum: 20 }
+  validates :name, length: { maximum: 20 }, uniqueness: { scope: :user }, presence: true
 end
