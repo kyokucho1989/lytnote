d2adc71672fd0494bdb369c94b9521ce58e405a3
ko <kyokucho1989@gmail.com>
2021-12-21 05:31:39 +0900

    genre-create test

--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -10,10 +10,10 @@ require 'rails_helper'
 
 RSpec.describe "Genres", type: :request do
   let(:user) { create(:user) }
-  let(:genre_count){3}
   before do
+
     sign_in user
-    create_list(:genre, genre_count, user: user)
+    create_list(:genre, 3, user: user)
   end
 
   describe "GET /genres" do
@@ -22,14 +22,21 @@ RSpec.describe "Genres", type: :request do
       subject
       res = JSON.parse(response.body)
       # binding.pry
-      expect(res["genres"].size).to eq genre_count
+      expect(res["genres"].size).to eq 3
       expect(res["genres"].first.keys).to eq ["id", "name"]
       expect(response).to have_http_status(200) 
     end
   end
 
   describe "POST /genres" do
+
+    subject { post(genres_path( :format => :json ), params: params) }
+    let(:params) { {genre: attributes_for(:genre) }}
+
     it "ジャンルのレコードが作成できる" do
+      binding.pry
+      expect { subject }.to change { Genre.count }.by(1)
+      expect(response).to have_http_status(200) 
     end
   end
 
