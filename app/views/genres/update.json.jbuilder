25f2002512f01c66d2c5860d2458913642d03534
ko <kyokucho1989@gmail.com>
2021-12-22 05:07:40 +0900

    genre update rspec追加

---
+++ app/views/genres/update.json.jbuilder
@@ -0,0 +1,2 @@
+json.id @genre.id
+json.name @genre.name
--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -9,19 +9,30 @@ require 'rails_helper'
 #             DELETE  /genres/:id(.:format)       genres#destroy
 
 RSpec.describe "Genres", type: :request do
-  let(:user) { create(:user) }
-  before do
+  # let(:user) { create(:user) }
 
-    sign_in user
-    create_list(:genre, 3, user: user)
+  before(:context) do
+    @user = create(:user)
+    create_list(:genre, 3, user: @user)
   end
 
+  before(:example) do
+    sign_in @user
+  end
+
+  after(:example) do
+    sign_out @user
+  end
+
+  after(:context) do
+    @user.destroy
+  end
+  
   describe "GET /genres" do
     subject { get(genres_path( :format => :json )) }
     it "ジャンル一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
-      # binding.pry
       expect(res["genres"].size).to eq 3
       expect(res["genres"].first.keys).to eq ["id", "name"]
       expect(response).to have_http_status(200) 
@@ -29,19 +40,24 @@ RSpec.describe "Genres", type: :request do
   end
 
   describe "POST /genres" do
-
     subject { post(genres_path( :format => :json ), params: params) }
     let(:params) { {genre: attributes_for(:genre) }}
-
     it "ジャンルのレコードが作成できる" do
-      binding.pry
       expect { subject }.to change { Genre.count }.by(1)
       expect(response).to have_http_status(200) 
     end
   end
 
-  describe "PATCH /genres/:id" do
+  describe "PUT /genres/:id" do
+
+    subject { put(genre_path(genre.id, :format => :json ), params: params) }
+    let(:params) { { genre: { name: Faker::Name.name, created_at: Time.current } } }
+    let(:genre) { create(:genre, user: @user) }
     it "任意のレコードを更新できる" do
+      # binding.pry
+      expect { subject }.to change { Genre.find(genre.id).name }.from(genre.name).to(params[:genre][:name]) &
+      not_change { Genre.find(genre.id).created_at }
+      expect(response).to have_http_status(200) 
     end
   end
 
@@ -50,7 +66,5 @@ RSpec.describe "Genres", type: :request do
     end
   end
 
-  after do
-    user.destroy
-  end
+
 end
