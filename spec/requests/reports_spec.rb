9ebcb63d83c635dfa0ce324612410c2c196f5c92
ko <kyokucho1989@gmail.com>
2021-12-25 05:42:33 +0900

    rspec

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,17 +1,52 @@
 require 'rails_helper'
 
+# reports     GET    /reports(.:format)           reports#index
+#             POST   /reports(.:format)           reports#create
+# new_report  GET    /reports/new(.:format)       reports#new
+# edit_report GET    /reports/:id/edit(.:format)  reports#edit
+# report      GET    /reports/:id(.:format)       reports#show
+#             PATCH  /reports/:id(.:format)       reports#update
+#             PUT    /reports/:id(.:format)       reports#update
+#             DELETE /reports/:id(.:format)       reports#destroy
+
 RSpec.describe "Reports", type: :request do
-  before do
+  before(:context) do
     @user = create(:user)
+    create_list(:report, 3, user: @user)
+  end
+
+  before(:example) do
+    sign_in @user
+  end
+
+  after(:example) do
+    sign_out @user
+  end
+
+  after(:context) do
+    @user.destroy
   end
 
   describe "GET /index" do
-    it "returns http success" do
-      sign_in @user
-      get reports_path
-      expect(response).to have_http_status(:success)
+    subject { get(reports_path( :format => :json )) }
+    it "日報一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res["reports"].size).to eq 3
+      expect(res["reports"].first.keys).to eq ["id", "content", "user", "reported_on"]
+      expect(response).to have_http_status(200) 
     end
   end
+
+  describe "POST /reports" do
+    subject { post(reports_path( :format => :json ), params: params) }
+    let(:params) { {report: attributes_for(:report, user_id:@user.id) }}
+    it "日報のレコードが作成できる" do
+      expect { subject }.to change { Report.count }.by(1)
+      expect(response).to have_http_status(200) 
+    end
+  end
+
 
   describe "GET /show" do
     it "returns http success" do
@@ -29,13 +64,7 @@ RSpec.describe "Reports", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      sign_in @user
-      get "/reports/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 
   describe "GET /edit" do
     it "returns http success" do
