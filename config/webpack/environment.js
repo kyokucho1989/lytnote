314c744cae9ccfb5cc764d8c6bec8ea1504d869b
ko <kyokucho1989@gmail.com>
2022-01-12 04:58:42 +0900

    nav var デザイン編集

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,10 +3,9 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-        // $: 'jquery/src/jquery',
-        // jQuery: 'jquery/src/jquery'
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
     })
 )
 
