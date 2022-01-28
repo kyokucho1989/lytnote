cf624f912e49c9fab3c4c88791fb30133d342e64
ko <kyokucho1989@gmail.com>
2022-01-29 05:08:31 +0900

    編集画面まで

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,6 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :ensure_normal_user, only: :create
-
+  binding.pry
   def ensure_normal_user
     if params[:user][:email].downcase == "guest@example.com"
       redirect_to new_user_session_path, alert: "ゲストユーザーのパスワード再設定はできません。"
