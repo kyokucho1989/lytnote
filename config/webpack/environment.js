2fa3f2033680668bdf591f9a56a8f70f8f3f7f9d
ko <kyokucho1989@gmail.com>
2022-05-06 11:47:02 +0900

    webpack エラー修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack');
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
