20b40b7135bea5214bdc138dce37681094a6c100
ko <kyokucho1989@gmail.com>
2022-05-06 14:32:32 +0900

    shファイル修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+# set -o errexit
 
-bundle install
+# bundle install
 
-yarn install
+# yarn install
 
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
