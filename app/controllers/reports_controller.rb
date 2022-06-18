dde239062faa706d56f2233c7fc1a78483a69d2a
ko <kyokucho1989@gmail.com>
2022-06-19 06:05:02 +0900

    ジャンル修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,14 +12,45 @@ class ReportsController < ApplicationController
   end
 
   def create
-    para = report_params[:report_items_attributes]
+    binding.pry
+
+#  ジャンル名登録
+# ジャンル名がDBにあるかどうか
+# あればそのジャンルidを返す
+# なければ新規ジャンル作成
+# そのジャンルidを返す
+
+
+
+
+    para = report_genre_params[:report_items_attributes]
+
+    report_lists = para.values
+    report_lists.each do |report|
+      genre_name = report[:genreset]
+      if genre_name.nil?
+        report[:genre_id] = nil
+        break
+      end
+      @genre_new = Genre.find_by(name:genre_name,user_id: current_user)
+      if @genre_new.nil?
+        @genre_new = Genre.new("name"=> genre_name, "user_id" => current_user.id)
+      end
+      report[:genre_id] = @genre_new.id
+    end
+
+    binding.pry
     first_key = para.keys.first
     first_value = para.values.first
     para.reject! { |_key, value| value[:content] == "" }
     para[first_key] = first_value unless para.key?("content")
+
+    para.values.map {|a| a.delete("genreset") }
+    binding.pry
     formatted_para = report_params
     formatted_para[:report_items_attributes] = para
     genres_set = get_genre_nameset
+
     share_content = Report.convert_content_shared(formatted_para, genres_set)
 
     @report = Report.new(formatted_para)
@@ -82,9 +113,23 @@ class ReportsController < ApplicationController
     end
   end
 
+  def get_genre_name(id)
+    if id.nil?
+      " "
+    else
+      genre = @genres.find{|array| array[:id] == id }
+      genre.name 
+    end   
+  end
+
+  helper_method :get_genre_name
   private
 
   def report_params
     params.require(:report).permit(:content, :reported_on, report_items_attributes: [:content, :genre_id, :work_hours, :content_for_share, :id])
+  end
+
+  def report_genre_params
+    params.require(:report).permit(:content, :reported_on, report_items_attributes: [:content, :genre_id, :genreset, :work_hours, :content_for_share, :id])
   end
 end
