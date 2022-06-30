42ea95ba3899313d80fbe5243474c648427aec2c
ko <kyokucho1989@gmail.com>
2022-07-01 04:18:02 +0900

    plan と report itemの制約変更

--- app/models/report_item.rb
+++ app/models/report_item.rb
@@ -1,6 +1,6 @@
 class ReportItem < ApplicationRecord
   belongs_to :report
-  belongs_to :genre
+  belongs_to :genre, optional: true
   validates :content, length: { maximum: 20 }, presence: true
   validates :work_hours, numericality: { greater_than: 0, less_than_or_equal_to: 24 }, length: { maximum: 4 }, presence: true
 end
