f87b091092ca70b377ab37e935e0e7868d507bdc
ko <kyokucho1989@gmail.com>
2022-10-13 05:18:19 +0900

    404エラー時のバックエンド処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     plans_path
-   end
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+    def record_not_found
+      render template: "layouts/error", status: 404
+      # render plain: "404 Not Found", status: 404
+    end
 end
