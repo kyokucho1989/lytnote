4ac38f72d5c5decf46ef2504479e706c930fdc46
ko <kyokucho1989@gmail.com>
2022-10-14 05:37:37 +0900

    viewでの分岐処理なくした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def status_color(status)
+      if status == "完了"
+        "bg-success"
+      elsif status == "中止"
+        "bg-dark text-white"
+      else
+        "d-none"
+      end
+    end
 end
