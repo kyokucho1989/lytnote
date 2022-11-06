e3a3770a56039ff07f7245387f25e9e0de27492b
ko <kyokucho1989@gmail.com>
2022-11-06 10:14:46 +0900

    planの初期状態を変更

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,6 +5,7 @@ class Plan < ApplicationRecord
   accepts_nested_attributes_for :review_items
   validates :deadline, presence: true
   validate :deadline_cannot_set_in_past
+  validate :cannot_edit_on_complete, on: :update
   validate :progress_total_count_cannot_set_exceeded_limit
   validates :name, length: { maximum: 20 }, presence: true
 
@@ -18,5 +19,11 @@ class Plan < ApplicationRecord
 
   def deadline_cannot_set_in_past
     errors.add(:deadline, ":ステータスが「進行中」の場合、過去の日付は使えません") if status == "進行中" && (deadline.present? && deadline < Date.today)
+  end
+
+  def cannot_edit_on_complete
+    if status == "完了" && (deadline_changed? || name_changed?)
+      errors.add(:deadline, ":完了状態で締め切りは変更できません") 
+    end
   end
 end
